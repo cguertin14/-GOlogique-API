02feb02c13c6d6d0ccccb5ec581312be07127106
Charles Morin <charlobino@hotmail.com>
2018-05-05 15:08:41 -0400

    Finished generateCoords

--- server/api/console/cli-routes.js
+++ server/api/console/cli-routes.js
@@ -1,7 +1,9 @@
 import RoutesHelper from './routesListing';
 import userRoutes from './../routes/users';
 import pointsRoutes from './../routes/points';
+import mapRoutes from './../routes/map';
 
 // List all routes.
 RoutesHelper.print('/users',  userRoutes.stack);
 RoutesHelper.print('/points', pointsRoutes.stack);
+RoutesHelper.print('/map', mapRoutes.stack);
--- server/api/routes/map.js
+++ server/api/routes/map.js
@@ -3,12 +3,8 @@ import { bearer } from './../middlewares/authenticate';
 import express from 'express';
 const router = express.Router();
 
-router.post('/gendata', async (req, res) => {
-    await new MapController(req,res).generateData();
-});
-
-router.post('/genpoints', async (req, res) => {
-    await new MapController(req, res).generatePoints(); 
+router.post('/gencoords', bearer, async (req, res) => {
+    await new MapController(req, res).generateCoords(); 
 });
 
 export default router;
