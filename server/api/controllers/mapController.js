40b5493d9994dc70768c9545815c660cfa06bee3
Charles Morin <charlobino@hotmail.com>
2018-05-05 09:46:22 -0400

    Almost finished generatePoints

--- server/api/controllers/mapController.js
+++ server/api/controllers/mapController.js
@@ -3,29 +3,51 @@ import { ObjectId } from 'mongodb';
 import BaseController from './baseController';
 import Google from './../../config/json/google.json';
 import axios from 'axios';
+import fs from 'fs';
+import geolib from 'geolib';
+import Path from 'path';
+import coords from './../../../data/grandsparcs.json';
 
 export default class MapController extends BaseController {
 
     async generateData() {
         try {
-            const payload = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${this.req.body.lat},${this.req.body.lng}&key=${Google.GEOLOCATION}`);
+            const addr = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${this.req.body.lat},${this.req.body.lng}&key=${Google.GEOLOCATION}`);
             const img = await axios.get(`https://maps.googleapis.com/maps/api/streetview?size=128x128&location=${this.req.body.lat},${this.req.body.lng}&heading=151.78&pitch=-0.76&key=${Google.STREET_VIEW}`);
-            var address = payload.data.results[0].formatted_address;
+            var address = addr.data.results[0].formatted_address;
             var image = img.config.url;
-            
+
             return this.res.json({ address, image });
-        } catch(e) {
-            console.log(e);
-            
+        } catch (e) {
             return this.res.status(400).send(e);
         }
     }
 
     async generatePoints() {
         try {
+            coords.forEach(element => {
+                
+            });
+            
+            let data = coords.filter(coordinate => {
+                if (geolib.isPointInCircle({
+                    latitude: coordinate[0],
+                    longitude: coordinate[1]
+                }, {
+                        latitude: this.req.body.lat,
+                        longitude: this.req.body.lng
+                    }, 5000)) {
+                    return coordinate;
+                }
+            });
 
-        } catch(e) {
+            if (data.length >= 5)
+                return this.res.json(data.slice(0,4));
+            else
+                return this.res.json(data);    
 
+        } catch (e) {
+            return this.res.status(400).send(e);
         }
     }
 }
--- server/api/routes/map.js
+++ server/api/routes/map.js
@@ -3,8 +3,12 @@ import { bearer } from './../middlewares/authenticate';
 import express from 'express';
 const router = express.Router();
 
-router.post('/', (req, res) => {
-    new MapController(req,res).generateData();
+router.post('/gendata', async (req, res) => {
+    await new MapController(req,res).generateData();
+});
+
+router.post('/genpoints', async (req, res) => {
+    await new MapController(req, res).generatePoints(); 
 });
 
 export default router;
